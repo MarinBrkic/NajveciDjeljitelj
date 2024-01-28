041ffd88483fc28d6eaeaf9b6ec10f53c0a53a98
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-28 12:35:27 +0100

    Add files via upload

---
+++ NajveciDjeljitelj.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi prvi broj: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("unesi drugi broj: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int gcd = FindGCD(num1, num2);
+            Console.WriteLine("Najveci djeljitelj oba broja je {0}", gcd);
+
+            Console.ReadKey();
+        }
+
+        static int FindGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
